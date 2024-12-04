55f44869754f416c8944e8c7c46f730389c48902
Abhishek <officialabhishekthakur@gmail.com>
2024-12-04 17:20:45 +0530

    added dark mode functionality in project 14

---
+++ src/14_Dark Mode/script.js
@@ -0,0 +1,21 @@
+const themeToggle = document.getElementById("theme-toggle");
+const container = document.getElementById("container");
+
+themeToggle.addEventListener("click", function () {
+  if (
+    container.classList.contains("bg-white") &&
+    container.classList.contains("text-black")
+  ) {
+    container.classList.remove("bg-white", "text-black");
+    container.classList.add("bg-black", "text-white");
+    themeToggle.classList.remove("fa-sun");
+    themeToggle.classList.add("fa-moon");
+  } else {
+    container.classList.remove("bg-black", "text-white");
+    container.classList.add("bg-white", "text-black");
+    themeToggle.classList.remove("fa-moon");
+    themeToggle.classList.add("fa-sun");
+  }
+});
+
+funcanality;
